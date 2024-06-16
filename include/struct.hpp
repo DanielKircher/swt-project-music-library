06f9e80134be6283f7794ad26493bed1b5149d68
DanielKircher <167426690+DanielKircher@users.noreply.github.com>
2024-06-16 20:57:42 +0200

    Struktur der Bibliothek

---
+++ include/struct.hpp
@@ -0,0 +1,13 @@
+#ifndef SONG_HPP
+#define SONG_HPP
+
+#include <string>
+
+struct Song {
+    std::string title;
+    std::string artist;
+    std::string album;
+    int year;
+};
+
+#endif // SONG_HPP
