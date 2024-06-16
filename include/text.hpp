b3e5e66b73e783379911a254a1d6fdf200a91b56
DanielKircher <167426690+DanielKircher@users.noreply.github.com>
2024-06-16 20:58:55 +0200

    Ausgelagerter Text

--- include/text.hpp
+++ include/text.hpp
@@ -1,53 +1,65 @@
 #include <iostream>
 
 void printWILLKOMMEN(){
-    std::cout << "\nWillkommen im Software-Technik-Projekt\n";
-    std::cout << "von Maximilian und Daniel\n\n";
+    std::cout << "\nWillkommen im Software-Technik-Projekt" << std::endl;
+    std::cout << "von Maximilian und Daniel" << std::endl << std::endl;
 }
 
 void printBEENDEN(){
-    std::cout << "Musikplayer wird beendet.\n";
+    std::cout << "Musikplayer wird beendet." << std::endl;
 }
 
 void printSTART(){
-    std::cout << "(1) Musikplayer starten.\n";
-    std::cout << "(0) Musikplayer beenden.\n\n";
+    std::cout << "(1) Musikplayer starten" << std::endl;
+    std::cout << "(0) Musikplayer beenden" << std::endl << std::endl;
 }
 
 void printHAUPTMENUE(){
-    std::cout << "===============Hauptmenü:===============\n";
-    std::cout << "(1) Suche\n";
-    std::cout << "(2) Lieblingssongs\n";
-    std::cout << "(3) Aktuelle Wiedergabe\n";
-    std::cout << "(0) Beenden\n\n";
+    std::cout << "=============== Hauptmenü ===============" << std::endl << std::endl;
+    std::cout << "(1) Suche" << std::endl;
+    std::cout << "(2) Lieblingssongs" << std::endl;
+    std::cout << "(3) Aktuelle Wiedergabe" << std::endl;
+    std::cout << "(0) Beenden" << std::endl << std::endl;
 }
 
 void printSUCHE(){
-    std::cout << "===============Suche:===============\n";
-    std::cout << "(1) In Cloud suchen\n";
-    std::cout << "(0) Zurück\n";
+    std::cout << "=============== Suche ===============" << std::endl << std::endl;
+    std::cout << "(1) In Cloud suchen" << std::endl;
+    std::cout << "(0) Zurück" << std::endl << std::endl;
+}
+
+void printRESULT(std::string searchResult, int play){
+    if (play == 1){
+        std::cout << "=============== " + searchResult + " ===============" << std::endl << std::endl;
+        std::cout << "(1) Stop" << std::endl;
+        std::cout << "(2) zu Lieblingssongs hinzufügen" << std::endl;
+        std::cout << "(0) Zurück" << std::endl << std::endl;
+    }
+    else{
+        std::cout << "=============== " + searchResult + " ===============" << std::endl << std::endl;
+        std::cout << "(1) Play" << std::endl;
+        std::cout << "(2) zu Lieblingssongs hinzufügen" << std::endl;
+        std::cout << "(0) Zurück" << std::endl << std::endl;
+    }
 }
 
 void printLIEBLINGSSONGS(){
-    std::cout << "\n===============Lieblingssongs:===============\n";
-    std::cout << "(1) Suche\n";
-    std::cout << "(2) Alle anzeigen\n";
-    std::cout << "(0) Zurück\n";
+    std::cout << "=============== Lieblingssongs ===============" << std::endl << std::endl;
+    std::cout << "(1) Suche"  << std::endl;
+    std::cout << "(2) Alle anzeigen" << std::endl;
+    std::cout << "(0) Zurück" << std::endl << std::endl;
 }
 
 void printWIEDERGABE(std::string actualSong){
-    std::cout << "\n===============Aktuelle Wiedergabe:===============\n";
-    std::cout << actualSong << std::endl;
-    std::cout << "(1) Letzter Song\n";
-    std::cout << "(3) Nächster Song\n";
-    std::cout << "(0) Zurück\n\n";
+        std::cout << "\n=============== Wiedergabe ===============" << std::endl << std::endl;
+    std::cout << "(1) Letzter Song << " + actualSong + " >> Nächster Song (3)" << std::endl;
+    std::cout << "(0) Zurück" << std::endl << std::endl;
 }
 
 void printNoWIEDERGABE(){
-    std::cout << "Keine Wiedergabe\n\n";
+    std::cout << "Keine Wiedergabe" << std::endl << std::endl;
 }
 
 void printFEHLER(){
-    std::cout << "FEHLER: Ungültige Eingabe\n\n";
+    std::cout << "FEHLER: Ungültige Eingabe" << std::endl << std::endl;
 }
-
